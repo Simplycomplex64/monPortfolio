0f23908f9999dbf8c68d7e3b1c9c121340a40014
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2023-12-14 16:55:36 -0500

    Add files via upload

---
+++ src/javascript/navigation.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('navigation/navigation.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navContainer').innerHTML = data;
+
+            // Get the current page's filename (e.g., "index.html" or "contact.html")
+            var currentPage = window.location.pathname.split('/').pop();
+
+            // Find the corresponding link and add the "active" class
+            var navLinks = document.querySelectorAll('#navContainer nav a');
+            navLinks.forEach(function (link) {
+                var linkPage = link.getAttribute('href').split('/').pop();
+                if (linkPage === currentPage) {
+                    link.classList.add('active');
+                }
+            });
+        });
+});
